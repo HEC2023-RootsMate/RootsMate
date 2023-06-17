89edb95336a19a80df008b21f2cdb4a25c715f9a
Robin Chavignat <r.chavignat@gmail.com>
2023-06-17 15:27:26 +0200

    First crappy feed page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,11 +6,13 @@ import { SigninComponent } from './pages/signin/signin.component';
 import { UserprofileComponent } from './pages/userprofile/userprofile.component';
 import { LocationsComponent } from './pages/locations/locations.component';
 import { LocationsDetailComponent } from './pages/locations/locations-detail/locations-detail.component';
+import { FeedComponent } from './pages/feed/feed.component';
 
 
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
+  { path: 'feed', component: FeedComponent },
   { path: 'signup', component: SignupComponent },
   { path: 'signin', component: SigninComponent },
   { path: 'userprofile', component: UserprofileComponent },
