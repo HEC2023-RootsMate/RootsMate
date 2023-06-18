e400bf482551b94804bd60c7cb871ee9c94beb50
lucas <lucas.weibel@gmail.com>
2023-06-18 11:14:13 +0200

    clean app.component & home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +6,13 @@ import { Component, ViewEncapsulation } from '@angular/core';
   styleUrls: ['./home.component.scss'],
   encapsulation: ViewEncapsulation.None,
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
   images = [
     {src: '/assets/images/nature/1.jpeg', text: 'Jardin tropical', owner: 'Coco'},
     {src: '/assets/images/nature/2.jpeg', text: 'Moment de détente', owner: 'Busra'},
     {src: '/assets/images/nature/3.jpeg', text: 'Terrain communautaire', owner: 'Lucas'}
   ];
+
+  ngOnInit(): void {
+  }
 }
