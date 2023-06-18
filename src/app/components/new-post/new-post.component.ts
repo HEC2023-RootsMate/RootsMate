b295dfacf49659b321eee2f0398d444dab50b10e
DAGLI Busra <busra.dagli@uha.fr>
2023-06-18 11:57:49 +0200

    Style feed new post part 1

--- src/app/components/new-post/new-post.component.ts
+++ src/app/components/new-post/new-post.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Post } from 'src/app/models/post.model';
+import { User } from 'src/app/models/user.model';
 import { PostsService } from 'src/app/services/posts.service';
 
 @Component({
@@ -7,10 +8,19 @@ import { PostsService } from 'src/app/services/posts.service';
   templateUrl: './new-post.component.html',
   styleUrls: ['./new-post.component.scss']
 })
-export class NewPostComponent {
+export class NewPostComponent implements OnInit {
   new_post: Post = new Post(0, '', '', '', new Date(), 0, 0, 0);
+  currentUser!: User;
 
-  constructor(private postsService: PostsService) { }
+  constructor(private postsService: PostsService) {
+  }
+
+  ngOnInit(): void {
+    const currentUserLocalStorage = localStorage.getItem('currentUser');
+    if(currentUserLocalStorage) {
+      this.currentUser = JSON.parse(currentUserLocalStorage)
+    }
+  }
 
   onSubmit() {
     this.postsService.add(this.new_post);
