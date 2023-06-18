92720fb89695e9fa22e0bb8a3b515f4c65497ff9
Robin Chavignat <r.chavignat@gmail.com>
2023-06-18 14:29:20 +0200

    Use User info in posts feed and fix errors

--- src/app/components/post-feed/post-feed.component.ts
+++ src/app/components/post-feed/post-feed.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Post } from 'src/app/models/post.model';
 import { PostsService } from 'src/app/services/posts.service';
+import { UsersService } from 'src/app/services/users.service';
 
 // TODO: figure out a way to automatically update the page contents when a post is submitted
 
@@ -12,9 +13,17 @@ import { PostsService } from 'src/app/services/posts.service';
 export class PostFeedComponent implements OnInit {
   all_posts: Post[] = [];
 
-  constructor(private postsService: PostsService) { }
+  constructor(private postsService: PostsService, private usersService: UsersService) { }
 
   ngOnInit() {
     this.all_posts = this.postsService.getAll();
+  }
+
+  getUsernameById(id: Number): string {
+    return this.usersService.getById(id)?.username ?? "unknown";
+  }
+
+  getAvatarUrlById(id: Number): string {
+    return this.usersService.getById(id)?.picture ?? "unknown";
   }
 }
